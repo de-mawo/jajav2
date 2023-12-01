0c15f8506714044f568ca62d6cc506d98176617b
de-mawo <groovebassist1@gmail.com>
2023-12-01 07:36:59 +0200

    Added more points to the rubic

--- data/matrix.ts
+++ data/matrix.ts
@@ -1,79 +1,83 @@
 const matrix = [
-  
-    {
-        id: 2,
-       score_title: 'Information about the problem',
-       
-    },
-    {
-        id: 3,
-       score_title: 'Value proposition   (how is the problem solved)',
-       
-    },
-    {
-        id: 4,
-       score_title: 'Target market   understanding',
-       
-    },
-    {
-        id: 5,
-       score_title: 'Competition and  competitive advantage',
-       
-    },
-    {
-        id: 6,
-       score_title: 'Spoke clearly (tone,  body language) ',
-      
-    },
-    {
-        id: 7,
-       score_title: 'Told a story (flow) ',
-       
-    },
-    ,
-    {
-        id: 8,
-       score_title: 'Related to the     audience ',
-       
-    },
-    ,
-    {
-        id: 9,
-       score_title: 'Appropriate    language ',
-       
-    },
-    ,
-    {
-        id: 10,
-       score_title: 'Passionate about the   idea',
-       
-    },
-    ,
-    {
-        id: 11,
-       score_title: 'Introduction and   closing ',
-       
-    },
-    ,
-    {
-        id: 12,
-       score_title: 'Visual presentation    (PowerPoint) ',
-       
-    },
-    {
-        id: 14,
-       score_title: 'Uniqueness of      Business idea  (Creative ideation   towards solutions)',
-       
-    },
-    ,
-    {
-        id: 15,
-       score_title: 'Business idea:   scalability ',
-       
-    },
-    
+  {
+    id: 2,
+    score_title: "Information about the problem",
+  },
+  {
+    id: 3,
+    score_title: "Value proposition   (how is the problem solved)",
+  },
+  {
+    id: 4,
+    score_title: "Target market   understanding",
+  },
+  {
+    id: 5,
+    score_title: "Competition and  competitive advantage",
+  },
+  {
+    id: 6,
+    score_title: "Spoke clearly (tone,  body language) ",
+  },
+  {
+    id: 7,
+    score_title: "Told a story (flow) ",
+  },
+  ,
+  {
+    id: 8,
+    score_title: "Related to the     audience ",
+  },
+  ,
+  {
+    id: 9,
+    score_title: "Appropriate    language ",
+  },
+  ,
+  {
+    id: 10,
+    score_title: "Passionate about the   idea",
+  },
+  ,
+  {
+    id: 11,
+    score_title: "Introduction and   closing ",
+  },
+  ,
+  {
+    id: 12,
+    score_title: "Visual presentation    (PowerPoint) ",
+  },
+  {
+    id: 14,
+    score_title:
+      "Uniqueness of      Business idea  (Creative ideation   towards solutions)",
+  },
 
+  {
+    id: 15,
+    score_title: "Business idea:   scalability ",
+  },
 
-]
+  {
+    id: 16,
+    score_title: "Market size",
+  },
 
-export default matrix
+  {
+    id: 17,
+    score_title: "Revenue model ",
+  },
+
+  {
+    id: 18,
+    score_title: "Financial projections ",
+  },
+
+  {
+    id: 19,
+    score_title: "Strength of the team",
+  },
+];
+
+export default matrix;
